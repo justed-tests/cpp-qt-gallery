22f87e722375fa97db5a61c4543bb7f0ccec092c
justed <hafizov@mag-soft.ru>
2017-03-10 15:05:06 +0500

    million definitions

--- gallery-desktop/AlbumWidget.cpp
+++ gallery-desktop/AlbumWidget.cpp
@@ -1,14 +1,86 @@
 #include "AlbumWidget.h"
 #include "ui_AlbumWidget.h"
 
+#include <QInputDialog>
+#include <QFileDialog>
+
+#include "AlbumModel.h"
+#include "PictureModel.h"
+
 AlbumWidget::AlbumWidget(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::AlbumWidget)
+  QWidget(parent),
+  ui(new Ui::AlbumWidget),
+  mAlbumModel(nullptr),
+  mAlbumSelectionModel(nullptr),
+  mPictureModel(nullptr),
+  mPictureSelectionModel(nullptr)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+  clearUi();
+
+  ui->thumbnailListView->setSpacing(5);
+  ui->thumbnailListView->setResizeMode(QListView::Adjust);
+  ui->thumbnailListView->setFlow(QListView::LeftToRight);
+  ui->thumbnailListView->setWrapping(true);
+
+  connect(
+    ui->thumbnailListView, &QListView::doubleClicked,
+    this,                  &AlbumWidget::pictureActivated
+  );
+
+  connect(
+    ui->deleteButton, &QPushButton::clicked,
+    this,             &AlbumWidget::deleteAlbum
+  );
+
+  connect(
+    ui->editButton, &QPushButton::clicked,
+    this,           &AlbumWidget::editAlbum
+  );
+
+  connect(
+    ui->addPicturesButton, &QPushButton::clicked,
+    this,                  &AlbumWidget::addPictures
+  );
 }
 
 AlbumWidget::~AlbumWidget()
 {
-    delete ui;
+  delete ui;
+}
+
+void AlbumWidget::setAlbumModel(AlbumModel* albumModel)
+{
+}
+
+void AlbumWidget::setAlbumSelectionModel(QItemSelectionModel* albumSelectionModel)
+{
+}
+
+void AlbumWidget::setPictureModel(ThumbnailProxyModel* pictureModel)
+{
+}
+
+void AlbumWidget::setPictureSelectionModel(QItemSelectionModel* selectionModel)
+{
+}
+
+void AlbumWidget::deleteAlbum()
+{
+}
+
+void AlbumWidget::editAlbum()
+{
+}
+
+void AlbumWidget::addPictures()
+{
+}
+
+void AlbumWidget::clearUi()
+{
+}
+
+void AlbumWidget::loadAlbum(const QModelIndex& albumIndex)
+{
 }
